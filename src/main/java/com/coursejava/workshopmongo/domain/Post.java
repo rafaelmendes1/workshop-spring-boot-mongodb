9f4f96a26f94291defac130a7f7beeeede59b1f6
rafaelmendes1 <rarafafa3245@gmail.com>
2020-07-24 17:10:15 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/coursejava/workshopmongo/domain/Post.java
+++ src/main/java/com/coursejava/workshopmongo/domain/Post.java
@@ -1,9 +1,12 @@
 package com.coursejava.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.coursejava.workshopmongo.dto.AuthorDTO;
+import com.coursejava.workshopmongo.dto.CommentDTO;
 
 public class Post implements Serializable {
 	
@@ -14,6 +17,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -68,6 +73,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -92,4 +105,5 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 }
